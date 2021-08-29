9b54ad5f17f3db14f8efc0839574375f3f5be8d8
EhWanu <cameronjshadgett@gmail.com>
2021-08-28 23:07:43 -0400

    leftside positioning

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { ArrowDropDownCircle, Notifications, Search } from '@material-ui/icons'
+import { ArrowDropDown, Notifications, Search } from '@material-ui/icons'
 import './navbar.scss'
 
 const Navbar = () => {
@@ -20,7 +20,7 @@ const Navbar = () => {
                     <span>KID</span>
                     <Notifications />
                     <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-                    <ArrowDropDownCircle/> 
+                    <ArrowDropDown />
                 </div>
             </div>
         </div>
