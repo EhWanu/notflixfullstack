ca32ba8400c5c55b65534dcffedc06b2df9cca28
EhWanu <cameronjshadgett@gmail.com>
2021-08-28 22:36:00 -0400

    Navbar Boiler Plate

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,11 @@
 import { AcUnit } from '@material-ui/icons'
+import Navbar from '../Components/Navbar/Navbar'
 import './home.scss'
 
 const Home = () => {
     return (
         <div className='home'>
-            <AcUnit/>
+            <Navbar/>
         </div>
     )
 }
