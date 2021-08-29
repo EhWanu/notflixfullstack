e040f16ed9a972515570f7e523247fe68ecf57e9
EhWanu <cameronjshadgett@gmail.com>
2021-08-28 22:40:36 -0400

    Logo and button layout start

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,19 @@ import './navbar.scss'
 const Navbar = () => {
     return (
         <div className="navbar">
-            navbar
+            <div className="container">
+                <div className="left">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png" 
+                    alt=""
+                    />
+                    <span>Homepage</span>                   
+                    <span>Series</span>                   
+                    <span>Movies</span>                   
+                    <span>New and Popular</span>                   
+                    <span>My List</span>                   
+                </div>
+                <div className="right"></div>
+            </div>
         </div>
     )
 }
